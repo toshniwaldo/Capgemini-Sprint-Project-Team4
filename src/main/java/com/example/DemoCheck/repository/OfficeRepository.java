69d20739890367a67f96849868ba983f7bf9a217
jatin-ukey21 <jatinukey1@gmail.com>
2026-03-26 15:33:22 +0530

    Update OfficeRepositoryTest: Added test case for returning empty list when data does not exists

--- src/main/java/com/example/DemoCheck/repository/OfficeRepository.java
+++ src/main/java/com/example/DemoCheck/repository/OfficeRepository.java
@@ -2,8 +2,15 @@ package com.example.DemoCheck.repository;
 
 import com.example.DemoCheck.entity.Office;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 @RepositoryRestResource(path = "offices")
 public interface OfficeRepository extends JpaRepository<Office, String> {
+
+    @RestResource(path = "by-cities")
+    List<Office> findByCityIn(@Param("cities") List<String> cities);
 }
